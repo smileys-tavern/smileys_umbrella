a678feaecb973da69a6d25acbebcaa5e44a2ff50
Tyler-pierce <tyler.pierce@gmail.com>
2017-08-26 20:48:43 -0700

    Bugfix for log out link

--- apps/smileys/mix.exs
+++ apps/smileys/mix.exs
@@ -4,7 +4,7 @@ defmodule Smileys.Mixfile do
   def project do
     [
       app: :smileys,
-      version: "0.0.2",
+      version: "0.0.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
