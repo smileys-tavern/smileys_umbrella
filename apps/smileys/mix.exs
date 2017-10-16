77897aa5eba1073c6cd932e09b0d563039c8252c
Tyler-pierce <tyler.pierce@gmail.com>
2017-10-16 06:47:45 -0700

    Moved smileys to version 0.0.7

--- apps/smileys/mix.exs
+++ apps/smileys/mix.exs
@@ -4,7 +4,7 @@ defmodule Smileys.Mixfile do
   def project do
     [
       app: :smileys,
-      version: "0.0.6",
+      version: "0.0.7",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
